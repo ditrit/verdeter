75fc676b30fa7d73315fee3a1fe63b1d10d859cc
Théophile Roos <slashformotion@protonmail.com>
2022-10-10 13:59:33 +0200

    Update tests

--- validators/AuthorizedValues_test.go
+++ validators/AuthorizedValues_test.go
@@ -12,7 +12,7 @@ func TestContains(t *testing.T) {
 	assert.False(t, contains(5, set))
 }
 
-func TestValueIn(t *testing.T) {
+func TestAuthorizedValuesInt(t *testing.T) {
 	set := []int{1, 2, 3, 4}
 	validator := AuthorizedValues("integer list", set...)
 	assert.NotNil(t, validator)
@@ -20,4 +20,24 @@ func TestValueIn(t *testing.T) {
 	assert.NoError(t, validator.Func(1))
 	assert.Error(t, validator.Func("12"))
 	assert.Error(t, validator.Func(5))
+}
+
+func TestAuthorizedValuesString(t *testing.T) {
+	set := []string{"1", "2", "3", "4"}
+	validator := AuthorizedValues("string list", set...)
+	assert.NotNil(t, validator)
+	assert.Equal(t, "string list", validator.Name)
+	assert.NoError(t, validator.Func("1"))
+	assert.Error(t, validator.Func("12"))
+	assert.Error(t, validator.Func(4))
+}
+
+func TestAuthorizedValuesUint(t *testing.T) {
+	set := []uint{1, 2, 3, 4}
+	validator := AuthorizedValues("uint list", set...)
+	assert.NotNil(t, validator)
+	assert.Equal(t, "uint list", validator.Name)
+	assert.NoError(t, validator.Func(uint(1)))
+	assert.Error(t, validator.Func("12"))
+	assert.Error(t, validator.Func(uint(5)))
 }
--- validators/strings_test.go
+++ validators/strings_test.go
@@ -8,57 +8,13 @@ import (
 )
 
 func TestCheckStringNotEmpty(t *testing.T) {
-	testCases := []struct {
-		desc  string
-		in    any
-		valid bool
-	}{
-		{
-			desc:  "valid: not empty",
-			in:    "whatever, hey kid what are you doing ?     ",
-			valid: true,
-		}, {
-			desc:  "invalid: only spaces",
-			in:    "     ",
-			valid: false,
-		}, {
-			desc:  "invalid: only tabs",
-			in:    "			",
-			valid: false,
-		}, {
-			desc:  "valid: only one char",
-			in:    "s",
-			valid: true,
-		}, {
-			desc:  "valid: one char and spaces",
-			in:    "                µ        ",
-			valid: true,
-		}, {
-			desc:  "valid: one char and tabs",
-			in:    "				µ		",
-			valid: true,
-		}, {
-			desc:  "valid: one char and spaces and tabs",
-			in:    "	  	 		 µ	 	",
-			valid: true,
-		}, {
-			desc:  "invalid: wrong input type",
-			in:    21,
-			valid: false,
-		}, {
-			desc:  "invalid: wrong input type",
-			in:    struct{ mohsdhf int }{mohsdhf: 25612564},
-			valid: false,
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			if tc.valid {
-				assert.NoError(t, validators.StringNotEmpty.Func(tc.in), "should not return an error")
-			} else {
-				assert.Error(t, validators.StringNotEmpty.Func(tc.in), "should return an error")
-			}
+	assert.NoError(t, validators.StringNotEmpty.Func("whatever, hey kid what are you doing ?     "))
+	assert.NoError(t, validators.StringNotEmpty.Func("				µ		"), "non empty string with tabs")
+	assert.NoError(t, validators.StringNotEmpty.Func("   µ     "), "non empty string with spaces")
+	assert.NoError(t, validators.StringNotEmpty.Func("s"))
 
-		})
-	}
+	assert.Error(t, validators.StringNotEmpty.Func("      "), "empty string with spaces")
+	assert.Error(t, validators.StringNotEmpty.Func("			"), "empty string with tabs")
+	assert.Error(t, validators.StringNotEmpty.Func(struct{ mohsdhf int }{mohsdhf: 25612564}))
+	assert.Error(t, validators.StringNotEmpty.Func(21))
 }
